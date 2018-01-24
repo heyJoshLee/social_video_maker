fa5b380c6e44bbbcd61495d51671428da0668700
Josh Lee <lee45276@gmail.com>
2018-01-24 19:55:45 +0800

    Add support for m4v videos

--- script.rb
+++ script.rb
@@ -35,26 +35,40 @@ CSV.foreach("list.csv") do |row|
   puts "Combining"
 
 
-system("ffmpeg -f" +  " concat:#{dir_name}/intro.mp4|#{dir_name}/video.mp4 " +  "-safe 0 -i  -c copy #{dir_name}/combined_files.mp4")
+  # system("ffmpeg -f" +  " concat:#{dir_name}/intro.mp4|#{dir_name}/video.mp4 " +  "-safe 0 -i  -c copy #{dir_name}/combined_files.mp4")
 
-system("mencoder -oac pcm -ovc x264  #{dir_name}intro.mp4 #{dir_name}video.mp4 outtros/facebook.m4v -o #{dir_name}full_movie_fb.mp4")
-system("mencoder -oac pcm -ovc x264  #{dir_name}intro.mp4 #{dir_name}video.mp4 outtros/youtube.m4v -o #{dir_name}full_movie_yt.mp4")
-
-# compress videos
-system("ffmpeg -i #{dir_name}full_movie_fb.mp4 -vcodec h264 -acodec aac -strict -2 #{dir_name}full_movie_compressed_fb.mp4")
-system("ffmpeg -i #{dir_name}full_movie_yt.mp4 -vcodec h264 -acodec aac -strict -2 #{dir_name}full_movie_compressed_yt.mp4")
+  #combine videos
 
 
-# delete html and uncompressed videos
-system("rm -f #{dir_name}full_movie_yt.mp4")
-system("rm -f #{dir_name}full_movie_fb.mp4")
-system("rm -f #{dir_name}intro.mp4")
-system("rm -f #{dir_name}image_in_html.html")
-system("rm -f #{dir_name}video_thumbnail.html")
+  # Facebook video
 
+  # Check video file type
+  if File.file?(dir_name + "video.mp4")
+    system("mencoder -oac pcm -ovc x264  #{dir_name}intro.mp4 #{dir_name}video.mp4 outtros/facebook.m4v -o #{dir_name}full_movie_fb.mp4")
+  elsif File.file?(dir_name + "video.m4v")
+    system("mencoder -oac pcm -ovc x264  #{dir_name}intro.mp4 #{dir_name}video.m4v outtros/facebook.m4v -o #{dir_name}full_movie_fb.mp4")
+  end
 
-count += 1
+  # Youtube video
+  # Check video file type
+
+  if File.file?(dir_name + "video.mp4")
+    system("mencoder -oac pcm -ovc x264  #{dir_name}intro.mp4 #{dir_name}video.mp4 outtros/youtube.m4v -o #{dir_name}full_movie_yt.mp4")
+  elsif File.file?(dir_name + "video.m4v")
+    system("mencoder -oac pcm -ovc x264  #{dir_name}intro.mp4 #{dir_name}video.m4v outtros/youtube.m4v -o #{dir_name}full_movie_yt.mp4")
+  end
+
+  # compress videos
+  system("ffmpeg -i #{dir_name}full_movie_fb.mp4 -vcodec h264 -acodec aac -strict -2 #{dir_name}full_movie_compressed_fb.mp4")
+  system("ffmpeg -i #{dir_name}full_movie_yt.mp4 -vcodec h264 -acodec aac -strict -2 #{dir_name}full_movie_compressed_yt.mp4")
+
+  # delete html and uncompressed videos
+  system("rm -f #{dir_name}full_movie_yt.mp4")
+  system("rm -f #{dir_name}full_movie_fb.mp4")
+  system("rm -f #{dir_name}intro.mp4")
+  system("rm -f #{dir_name}image_in_html.html")
+  system("rm -f #{dir_name}video_thumbnail.html")
+
+  count += 1
 
 end
-
-puts "Finished"
